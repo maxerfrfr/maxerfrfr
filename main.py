a8056e3aabdff7a004bbe5a1b708f7d39edc6298
maxerfrfr <liviudulap000@gmail.com>
2023-09-10 13:26:03 +0300

    main

---
+++ main.py
@@ -0,0 +1,69 @@
+
+#print(10+10)
+#print(5/5)
+
+#a = input ("scire numele tau")
+
+#print ("SALUT "+ a )
+
+#int = -4
+#float = 3.14
+#string = "My text"
+#boolean = True #False
+#print("Salut\n" + "Lume\n")
+
+#print(str(int) + str(float) + str(string) + str(boolean))
+
+#print(not boolean)
+#print(boolean or False)
+#print(boolean and False)
+
+
+#a = int(input( "scie varsta"))
+
+#if a > 18:      #19
+    #print ("U are major")
+#elif a ==18:
+    #print("cong")
+#else:
+   #print("Deny")
+
+
+#code .....
+
+
+
+
+
+
+
+#a = 0
+
+#while a != 111:
+    #print(a)
+    #a = a + 0.4
+#print("finish")
+
+
+
+
+#for e in range(1, 100):
+   # print(e)
+
+
+
+
+
+
+import random
+
+check = random.randint(1,  45)
+value = int(input("Write a number\n"))
+
+while check != value:
+    if value > check: print ("More than need\n")
+    else: print("Less than need\n")
+
+
+    value = int(input("Write another number\n"))
+print("U win\n")
